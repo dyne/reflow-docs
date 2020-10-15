e3b0b97c763934889f349d83105433473e748e31
ivan minutillo <bernini@inventati.org>
2020-10-15 16:23:01 +0200

    update siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'ReflowOS', // Title for your website.
   tagline: 'Architecture and Manual for Distributed Network Setup and Maintenance',
-  url: 'https://dyne.github.io', // Your website URL
-  baseUrl: '/reflowos/', // Base URL for your project */
+  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'reflowos',
+  projectName: 'ReflowOS',
   organizationName: 'dyne',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
